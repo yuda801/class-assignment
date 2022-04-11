4d0c57f91d234e01d544c88f43f8ea3782bd7b1a
YUDA RECHT <yudar801@gmail.com>
2022-04-11 21:42:25 +0300

    that's what I got

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Home from './components/Home.js';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+// import ErrorPage from './components/Error.js';
+// import AppBar from './components/AppBar.js'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        {/* <AppBar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/medicines" element={<Medicines />} /> */}
+          {/* <Route path="/createNewMedicine" element={<CreateNewMedicine />} /> */}
+          {/* <Route
+            path="*"
+            element={<ErrorPage to="/" />}
+          /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
